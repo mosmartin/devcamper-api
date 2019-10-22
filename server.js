91cc3eea9bb6738bd5657c3bf0fe6410c6b2dbb1
Martin Macharia <mosmartin5@gmail.com>
2019-10-22 17:21:59 +0200

    Update server.js
    Add the bootcamp routes to the server.

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 
+// require the route handlers
+const bootcamps = require("./src/routes/bootcamps");
+
 const app = express();
 
 const PORT = process.env.PORT;
+
+// mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 app.listen(
   PORT,
