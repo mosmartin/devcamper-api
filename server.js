292236e76bc5e38d546083fcf02c8def88873fa6
Martin Macharia <mosmartin5@gmail.com>
2019-10-22 21:17:33 +0200

    Update server.js
    Add error handling for unhandled promises.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const dbConnect = require('./config/db');
+const dbConnect = require("./config/db");
 
 // require the route handlers
 const bootcamps = require("./src/routes/bootcamps");
@@ -13,7 +13,7 @@ dbConnect();
 
 // dev logging middleware
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 const PORT = process.env.PORT;
@@ -21,7 +21,15 @@ const PORT = process.env.PORT;
 // mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`ERROR: ${err.message}`);
+
+  // close server and exit the process
+  server.close(() => process.exit(1));
+});
