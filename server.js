3e677346de421922f24012829a5bbc35889bc00c
Martin Macharia <mosmartin5@gmail.com>
2019-10-22 18:45:19 +0200

    Update server.js
    Add a dev logger.

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 // require the route handlers
 const bootcamps = require("./src/routes/bootcamps");
 
 const app = express();
+
+// dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT;
 
