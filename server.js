b26ee7f84021bb46ea904e63691da656ed633b17
Martin Macharia <mosmartin5@gmail.com>
2019-10-26 17:11:26 +0200

    Update server.js
    Add the error handler middleware.

--- server.js
+++ server.js
@@ -4,25 +4,31 @@ const morgan = require("morgan");
 const debug = require("debug")("worker:server");
 require("colors");
 require("supports-color");
+const errorHandler = require("./src/middleware/errors");
 const dbConnect = require("./config/db");
 
 // require the route handlers
 const bootcamps = require("./src/routes/bootcamps");
 
 const app = express();
+const PORT = process.env.PORT;
 
 // connect to the database
 dbConnect();
+
+// use the body parser middleware
+app.use(express.json());
 
 // dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-const PORT = process.env.PORT;
-
 // mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+
+// error handling middleware
+app.use(errorHandler);
 
 const server = app.listen(
   PORT,
@@ -33,9 +39,9 @@ const server = app.listen(
 );
 
 // handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-  debug(`ERROR: ${err.message}`);
+// process.on("unhandledRejection", (err, promise) => {
+//   debug(`ERROR: ${err.message}`);
 
-  // close server and exit the process
-  server.close(() => process.exit(1));
-});
+//   // close server and exit the process
+//   server.close(() => process.exit(1));
+// });
