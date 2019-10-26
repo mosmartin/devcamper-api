c0cc40b5f03c75e3612d707dc95f2895445941ba
Martin Macharia <mosmartin5@gmail.com>
2019-10-26 20:33:26 +0200

    Update errors.js
    Catch errors.

--- src/middleware/errors.js
+++ src/middleware/errors.js
@@ -1,13 +1,36 @@
 const debug = require("debug")("worker:errors-middleware");
 require("supports-color");
+const ErrorResponse = require("../helpers/ErrorResponse");
 
 const errorHandler = (err, req, res, next) => {
-  // log to console
-  debug(err.stack);
+  let error = { ...err };
 
-  res.status(err.statusCode || 500).json({
+  error.message = err.message;
+
+  // log to console
+  debug(err);
+
+  // mongoose bad object id
+  if (err.name === "CastError") {
+    const message = `Resource with id: ${err.value} not found.`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered.`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // mongoose validation errors
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || "Internal Server Error"
+    error: error.message || "Internal Server Error"
   });
 };
 
