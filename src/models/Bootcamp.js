81acf47259c8df434a74179d1b69810c59c960ee
Martin Macharia <mosmartin5@gmail.com>
2019-10-28 22:05:13 +0100

    Update Bootcamp.js
    Update the website validation pattern.

--- src/models/Bootcamp.js
+++ src/models/Bootcamp.js
@@ -25,7 +25,7 @@ const bootcampSchema = new Schema({
   website: {
     type: String,
     match: [
-      /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm,
+      /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
       "Please enter a valid web address."
     ]
   },
