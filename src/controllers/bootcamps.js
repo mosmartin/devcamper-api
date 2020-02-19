5365483652e682c9e600db1dcc56d65274ae0bf0
Martin Macharia <mosmartin5@gmail.com>
2020-02-19 16:46:55 +0100

    Add select and sorting to bootcamps

--- src/controllers/bootcamps.js
+++ src/controllers/bootcamps.js
@@ -15,15 +15,15 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // fields to exclude during filtering
-  const removeFields = ['select'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   // loop over removeFields and delete from the request query
   removeFields.forEach(param => delete reqQuery[param]);
 
   // create a query string
-  let queryStr = JSON.stringify(req.query);
+  let queryStr = JSON.stringify(reqQuery);
 
-  // add operators to the quert string
+  // add operators to the query string
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
   // build the query
@@ -33,6 +33,15 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
+  }
+
+  // sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    // sort by createdAt date descending
+    query = query.sort('-createdAt');
   }
 
   // execute the query
